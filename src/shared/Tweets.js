dbb6cf3e48d2fbe9f51e38a557e7839230881c8b
srishtiwadhera <53931980+srishtiwadhera@users.noreply.github.com>
2020-07-31 18:21:46 +0530

    Add files via upload

---
+++ src/shared/Tweets.js
@@ -0,0 +1,14 @@
+export const TWEETS = [
+    {
+        title: "My first tweet",
+        image: "/images/img1.png"
+    },
+    {
+        title: "My second tweet",
+        image: "/images/img2.png"
+    },
+    {
+        title: "My third tweet",
+        image: "/images/img3.png"
+    }
+]
